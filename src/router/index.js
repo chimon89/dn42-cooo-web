2ebee863c66b8c9417ffcb754e1aaf3344914ce5
Chimon89 <chimon@mail.dailypics.cn>
2022-05-31 01:06:14 +0800

    set history route

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
+import { createRouter, createWebHistory } from 'vue-router'
 import Main from "../views/Main.vue"
 import Home from "../views/Pages/Home.vue"
 import NodeInfo from "../views/Nodes/NodeInfo.vue";
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHashHistory(),
+  history: createWebHistory(),
   routes
 })
 
